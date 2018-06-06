c9e04a7f88c21fc625e60bffef326cee92add736
Abe Massry <amassry@linode.com>
2018-06-05 22:55:44 -0400

    Update and rename calc19.js to calc20.js

--- calc20.js
+++ calc20.js
@@ -68,13 +68,13 @@
                 shakes[person].push(dayStamp);
                 console.log(shakes);
                 shakes[person] = uniq_fast(shakes[person]);
-                if (requestCounter === peopleTotal) {
-                  for (var i = 0; i++; i<peopleTotal) {
-                    $('#running-calc').append('<h4>' + peopleArray[i] + ' shakes: '+ shakes[peopleArray[i]] +'</h4>');
-                  }
-                }
-                console.log(shakes);
               }
+              if (requestCounter === peopleTotal) {
+                for (var i = 0; i++; i<peopleTotal) {
+                  $('#running-calc').append('<h4>' + peopleArray[i] + ' shakes: '+ shakes[peopleArray[i]] +'</h4>');
+                }
+              }
+              console.log(shakes);
             });
           });
         });
